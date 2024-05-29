59498d82c80dd1c1701174dada0b699aa9e5580d
sohail amjad <sohail.amjad@tarento.com>
2024-05-29 15:18:51 +0530

    added spec

--- project/ws/app/src/lib/routes/home/routes/request/request-service.service.spec.ts
+++ project/ws/app/src/lib/routes/home/routes/request/request-service.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { RequestServiceService } from './request-service.service';
+import { RequestServiceService } from './request-service.service'
 
 describe('RequestServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({}))
 
   it('should be created', () => {
-    const service: RequestServiceService = TestBed.get(RequestServiceService);
-    expect(service).toBeTruthy();
-  });
-});
+    const service: RequestServiceService = TestBed.get(RequestServiceService)
+    expect(service).toBeTruthy()
+  })
+})
